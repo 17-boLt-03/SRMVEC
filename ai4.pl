88b563510f62356489c6aec928bdc55dfdafddf1
17-boLt-03 <159886148+17-boLt-03@users.noreply.github.com>
2025-11-18 18:06:28 +0000

    Add road definitions and route logic in ai4.pl

---
+++ ai4.pl
@@ -0,0 +1,13 @@
+road("tampa","houston",200).
+road("gordon","tampa",300).
+road("houston","gordon",100).
+road("houston","kansas_city",120).
+road("gordon","kansas_city",130).
+
+route(T1, T2, D) :-
+    road(T1, T2, D).
+
+route(T1, T2, D) :-
+    road(T1, X, D1),
+    route(X, T2, D2),
+    D is D1 + D2.
