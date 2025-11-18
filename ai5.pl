89661e1773a26997b2ff551a2d6dd7375670544f
17-boLt-03 <159886148+17-boLt-03@users.noreply.github.com>
2025-11-18 18:52:57 +0000

    Refactor 8-Puzzle BFS Solver code structure

--- ai5.pl
+++ ai5.pl
@@ -1,35 +1,26 @@
-% ---------- Move Definitions ----------
+% -------- 8-Puzzle BFS Solver --------
+
 move(State, NewState) :- move_left(State, NewState).
 move(State, NewState) :- move_right(State, NewState).
 move(State, NewState) :- move_up(State, NewState).
 move(State, NewState) :- move_down(State, NewState).
-
 move_left([0,A,B|T],   [A,0,B|T]).
 move_left([A,0,B|T],   [A,B,0|T]).
 move_left([A,B,C|T],   [A,B,C|T1]) :- move_left(T, T1).
-
 move_right([A,B,0|T],  [A,0,B|T]).
 move_right([A,0,B|T],  [0,A,B|T]).
 move_right([A,B,C|T],  [A,B,C|T1]) :- move_right(T, T1).
-
 move_up([0,A,B,C|T],   [C,A,B,0|T]).
 move_up([H|T],         [H|T1])      :- move_up(T, T1).
-
 move_down([A,B,C,0|T], [0,B,C,A|T]).
 move_down([H|T],       [H|T1])      :- move_down(T, T1).
-
-% ---------- Goal Check ----------
 goal([1,2,3,4,5,6,7,8,0]).
-
-% ---------- Puzzle BFS Solver ----------
 puzzle(State) :-
     bfs([[State]], [], SolutionPath),
     reverse(SolutionPath, OrderedPath),
     write_solution(OrderedPath, 0).
-
 bfs([[State|Path]|_], _, [State|Path]) :-
     goal(State), !.
-
 bfs([[State|Path]|Rest], Visited, Solution) :-
     findall(
         [NextState, State | Path],
@@ -41,8 +32,6 @@ bfs([[State|Path]|Rest], Visited, Solution) :-
     ),
     append(Rest, NewPaths, Queue),
     bfs(Queue, [State|Visited], Solution).
-
-% ---------- Output ----------
 write_solution([], _).
 write_solution([State|Rest], N) :-
     format("Step ~w:\n", [N]),
@@ -50,13 +39,12 @@ write_solution([State|Rest], N) :-
     nl,
     N1 is N + 1,
     write_solution(Rest, N1).
-
 print_state([]).
 print_state([A,B,C|T]) :-
     format("~w ~w ~w\n", [A,B,C]),
     print_state(T).
-
-% ---------- Entry Point ----------
 start :-
     InitialState = [1,2,3,4,5,6,0,7,8],   % Change this to any solvable state
     puzzle(InitialState).
+    
+?- start.
