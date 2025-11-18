96741c5f6c710356c73e7ab9bdfec2412064d6bb
17-boLt-03 <159886148+17-boLt-03@users.noreply.github.com>
2025-11-18 18:09:33 +0000

    Create ai3.pl

---
+++ ai3.pl
@@ -0,0 +1,20 @@
+% Facts
+child(a,b).   % b is child of a
+child(a,c).   % c is child of a
+child(a,d).   % d is child of a
+child(b,e).   % e is child of b
+child(b,f).   % f is child of b
+child(c,g).   % g is child of c
+
+% Rule to find path from A to G
+path(A, G, [A|Z]) :-
+    childnode(A, G, Z).
+
+% Direct child → path ends
+childnode(A, G, [G]) :-
+    child(A, G).
+
+% Recursive case
+childnode(A, G, [X|L]) :-
+    child(A, X),
+    childnode(X, G, L).
