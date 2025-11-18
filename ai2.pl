b904807e6cb6a7f0844ee0e0611e3a3e3c5075e3
17-boLt-03 <159886148+17-boLt-03@users.noreply.github.com>
2025-11-18 18:46:03 +0000

    Improve comments in 8 Queens problem solution

--- ai2.pl
+++ ai2.pl
@@ -1,3 +1,5 @@
+% -------- 8 Queens Problem (Backtracking Solution) --------
+
 solution([]).
 solution([c(X,Y)|Others]) :-
     solution(Others),
@@ -6,12 +8,13 @@ solution([c(X,Y)|Others]) :-
 
 nonattack(_, []).
 nonattack(c(X,Y), [c(X1,Y1)|Others]) :-
-    Y =\= Y1,                             % not same row
-    abs(Y1 - Y) =\= abs(X1 - X),          % not same diagonal
+    Y =\= Y1,                             % Not same row
+    abs(Y1 - Y) =\= abs(X1 - X),          % Not same diagonal
     nonattack(c(X,Y), Others).
 
 member(X, [X|_]).
 member(X, [_|T]) :-
     member(X, T).
 
-
+% -------- Goal --------
+?- solution([c(1,A), c(2,B), c(3,C), c(4,D), c(5,E), c(6,F), c(7,G), c(8,H)]).
